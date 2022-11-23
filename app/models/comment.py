f1dc435b0aeaba42b68fde22864ba463733e4b09
Jon Park <jonpark13@gmail.com>
2022-11-23 18:39:36 -0500

    rem timestamps on likes

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,4 +1,5 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
 
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -10,7 +11,7 @@ class Comment(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
     post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("posts.id")))
     comment = db.Column(db.String(250))
-    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    created_on = db.Column(db.DateTime, server_default=db.func.now(), default=datetime.utcnow())
     updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
     user = db.relationship("User", back_populates="comments")
