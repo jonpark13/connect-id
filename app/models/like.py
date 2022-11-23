000fab39e6c51929f274a4e0d6a75fa89234d2b8
Jon Park <jonpark13@gmail.com>
2022-11-23 01:59:30 -0500

    changes to models

--- app/models/like.py
+++ app/models/like.py
@@ -1,9 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 
-
-class Like(db.Model, UserMixin):
+class Like(db.Model):
     __tablename__ = 'likes'
 
     if environment == "production":
