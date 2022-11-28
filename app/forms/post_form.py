a5b3a1a86136be3152725d22ae29e06f85147487
Jon Park <jonpark13@gmail.com>
2022-11-28 13:23:39 -0500

    length validators

--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class PostForm(FlaskForm):
-    post_body = TextField("Post", validators=[DataRequired()])
+    post_body = TextField("Post", validators=[DataRequired(),Length(min=1, max=500)])
     images = TextField("images")
     user_id = IntegerField('user id', validators=[DataRequired()])
