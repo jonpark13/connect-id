93a478c4b1ef86c2420857c7ce8371f1e6ba4f0e
Jon Park <jonpark13@gmail.com>
2022-11-29 11:53:29 -0500

    navbar css

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,35 +1,40 @@
 
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import UserOptions from './UserOptions';
 
 const NavBar = () => {
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.session)
+
   return (
     <div>
       <div className='navBar' style={{display:"flex", flexDirection:"reverse-row", justifyContent: 'center'}}>
-        <div className='navBarLinks' style={{margin:"10px 5px"}}>
+        <div className='navBarLinks' style={{margin:"10px 5px", width:"60px"}}>
           <NavLink style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center", color:'black', textDecoration:'none'}} to='/feed' exact={true} activeClassName='active'>
             <i className="fa-solid fa-house-chimney" /><div>Home</div>
           </NavLink>
         </div>
-        <div className='navBarLinks' style={{margin:"10px 5px"}}>
+        <div className='navBarLinks' style={{margin:"10px 5px", width:"60px"}}>
           <NavLink style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center", color:'black', textDecoration:'none'}} to='/login' exact={true} activeClassName='active'>
             <i className="fa-solid fa-user-group" /><div>Login</div>
           </NavLink>
         </div>
-        <div className='navBarLinks' style={{margin:"10px 5px"}}>
+        <div className='navBarLinks' style={{margin:"10px 5px", width:"60px"}}>
           <NavLink style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center", color:'black', textDecoration:'none'}} to='/sign-up' exact={true} activeClassName='active'>
           <i className="fa-regular fa-message" /><div>Sign up</div>
           </NavLink>
         </div>
-        <div className='navBarLinks' style={{margin:"10px 5px"}}>
+        <div className='navBarLinks' style={{margin:"10px 5px", width:"60px"}}>
           <NavLink style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center", color:'black', textDecoration:'none'}} to='/test' exact={true} activeClassName='active'>
           <i className="fa-solid fa-house-chimney" /><div>Test</div>
           </NavLink>
         </div>
-        <div>
-          <LogoutButton />
-        </div>
+        {user && 
+            <UserOptions user={user.user} />
+        }
       </div>
     </div>
   );
